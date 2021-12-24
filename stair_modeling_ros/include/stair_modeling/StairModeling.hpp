af040d55250686c6744c7c05ba32e37e5e7484fc
matheecs <matheecs@qq.com>
2021-12-24 12:09:26 +0800

    add stair_modeling_ros package

---
+++ stair_modeling_ros/include/stair_modeling/StairModeling.hpp
@@ -0,0 +1,360 @@
+#ifndef STAIRMODELING_HPP_
+#define STAIRMODELING_HPP_
+
+#include <dynamic_reconfigure/server.h>
+#include <geometry_msgs/Point32.h>
+#include <geometry_msgs/PolygonStamped.h>
+#include <jsk_recognition_msgs/PolygonArray.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <message_filters/sync_policies/exact_time.h>
+#include <message_filters/time_synchronizer.h>
+#include <nav_msgs/Odometry.h>
+#include <pcl/common/common.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pluginlib/class_list_macros.h>
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+
+#include <Eigen/Eigen>
+#include <chrono>
+
+#include "plane_msg/Plane.h"
+#include "plane_msg/VecPlane.h"
+#include "stair_info_msg/stair_info.h"
+#include "stair_modeling/StairPerception.hpp"
+
+using namespace std;
+using namespace std::chrono;
+
+enum PolygonShowMode { polygon_line_mode = 0, polygon_surface_mode };
+
+class StairModeling {
+ public:
+  explicit StairModeling(
+      const ros::NodeHandle &private_nh = ros::NodeHandle("~"))
+      : private_nh(private_nh),
+        queueSize(5),
+        count(0),
+        has_stair(false),
+        height(0),
+        depth(0),
+        hd(0),
+        vd(0),
+        time_window(2) {}
+
+  void init() {
+    ROS_INFO("############# StairModeling start #############");
+
+    pub_stair_info =
+        private_nh.advertise<stair_info_msg::stair_info>("stair_info", 5);
+    pub_stair_poly = private_nh.advertise<jsk_recognition_msgs::PolygonArray>(
+        "stair_polygon_array", 1);
+
+    time_record.open("modeling_time_record.csv");
+    time_record << "TIME_STAMP"
+                << ","
+                << "pre-process"
+                << ","
+                << "pre-modeling"
+                << ","
+                << "stair-modeling" << endl;
+
+    stair_record.open("modeling_result_record.csv");
+    stair_record << "TIME_STAMP"
+                 << ","
+                 << "height"
+                 << ","
+                 << "depth"
+                 << ","
+                 << "h_dis"
+                 << ","
+                 << "v_dis" << endl;
+
+    output_record.open("modeling_output_record.csv");
+    output_record << "TIME_STAMP"
+                  << ","
+                  << "height"
+                  << ","
+                  << "depth"
+                  << ","
+                  << "h_dis"
+                  << ","
+                  << "v_dis" << endl;
+  }
+
+  // Do not use Eigen
+  float fromQuaternion2yaw(Eigen::Quaternionf q) {
+    float yaw =
+        atan2(2 * (q.x() * q.y() + q.w() * q.z()),
+              q.w() * q.w() + q.x() * q.x() - q.y() * q.y() - q.z() * q.z());
+    return yaw;
+  }
+
+  void planeOdomCallback(const plane_msg::VecPlane &vec_plane,
+                         const nav_msgs::OdometryConstPtr &odom) {
+    Eigen::Vector3f camera_position;
+
+    // Step1 get pose of camera
+    {
+      Eigen::Quaternionf body_rotation_q = Eigen::Quaternionf(
+          odom->pose.pose.orientation.w, odom->pose.pose.orientation.x,
+          odom->pose.pose.orientation.y, odom->pose.pose.orientation.z);
+      Eigen::Matrix3f body_rotation_m = body_rotation_q.toRotationMatrix();
+      Eigen::Matrix4f body2world;  // from world to body
+      body2world.block<3, 3>(0, 0) = body_rotation_m;
+      body2world(0, 3) = odom->pose.pose.position.x;
+      body2world(1, 3) = odom->pose.pose.position.y;
+      body2world(2, 3) = odom->pose.pose.position.z;
+      body2world(3, 3) = 1.0;
+
+      Eigen::Matrix4f cam2body;  // from body to cam
+      Eigen::Quaternionf cam2body_rotation_q = Eigen::Quaternionf(1, 0, 0, 0);
+      cam2body.block<3, 3>(0, 0) = cam2body_rotation_q.toRotationMatrix();
+      cam2body(0, 3) = -0.009375589;
+      cam2body(1, 3) = 0.015890727;
+      cam2body(2, 3) = 0.028273059;
+      cam2body(3, 3) = 1.0;
+
+      Eigen::Matrix4f cam_world = body2world * cam2body;
+      camera_position(0) = cam_world(0, 3);
+      camera_position(1) = cam_world(1, 3);
+      camera_position(2) = cam_world(2, 3);
+      Eigen::Quaternionf camera_rotation_q =
+          Eigen::Quaternionf(cam_world.block<3, 3>(0, 0));
+
+      // update down_direction, right_direction, forward_direction
+      stair_detector.down_direction = Eigen::Vector3f(0, 0, -1);
+
+      float yaw = fromQuaternion2yaw(camera_rotation_q);
+      Eigen::Quaternionf q = Eigen::AngleAxisf(yaw, Eigen::Vector3f::UnitZ()) *
+                             Eigen::AngleAxisf(0, Eigen::Vector3f::UnitY()) *
+                             Eigen::AngleAxisf(0, Eigen::Vector3f::UnitX());
+      stair_detector.forward_direction =
+          q.toRotationMatrix() * Eigen::Vector3f(1, 0, 0);
+
+      stair_detector.right_direction =
+          stair_detector.down_direction.cross(stair_detector.forward_direction);
+      stair_detector.right_direction.normalize();
+    }
+    // Step2 stair modeling
+    std::vector<double> vectime;
+
+    count++;
+    stair_perception::Stair stair;
+    if (!vec_plane.vecPlane.empty()) {
+      std::ostringstream oss;
+      oss.precision(3);
+      vsp_plane.clear();
+
+      auto t1 = system_clock::now();
+
+      convertCloud(vec_plane, vsp_plane, camera_position);
+
+      auto t2 = system_clock::now();
+      auto duration = duration_cast<microseconds>(t2 - t1);
+      ROS_INFO("Convert cloud time: %.2f ms", duration.count() / 1000.0);
+      oss << "Convert cloud time: " << duration.count() / 1000.0 << " ms"
+          << std::endl;
+
+      has_stair = stair_detector.process(vsp_plane, stair, keyInfo, vectime);
+
+      auto t3 = system_clock::now();
+      duration = duration_cast<microseconds>(t3 - t2);
+      ROS_INFO("Stair modeling time: %.2f ms",
+               float(duration.count()) / 1000.0);
+      oss << "Stair modeling time: " << duration.count() / 1000.0 << " ms"
+          << std::endl;
+
+      if (has_stair) {
+        ROS_INFO("Find stair");
+        detial_stair_model = stair_detector.getDetialStairModelString(stair);
+        est_stair_param = stair_detector.getEstimatedParamString(stair);
+
+        stair_record << vec_plane.vecPlane[0].cloud.header.stamp << ",";
+        stair_record << stair_detector.getEstimatedParamStringRcd(stair);
+
+        send2robot(stair, vec_plane.vecPlane[0].cloud.header.stamp);
+
+        publish_rviz_stair(vsp_plane, vec_plane.vecPlane[0].cloud.header.stamp);
+      } else {
+        detial_stair_model = "No stair";
+        est_stair_param = "No stair";
+      }
+
+      time_record << vec_plane.vecPlane[0].cloud.header.stamp << ",";
+      for (auto &t : vectime) time_record << t << ",";
+      time_record << std::endl;
+
+      running_time_str = oss.str();
+    }
+  }
+
+  void convertCloud(const plane_msg::VecPlane &vec_plane,
+                    std::vector<stair_perception::Plane> &vsp_plane,
+                    Eigen::Vector3f camera_position) {
+    unsigned long plane_number = vec_plane.vecPlane.size();
+    vsp_plane.resize(plane_number);
+
+    for (size_t i = 0; i < plane_number; i++) {
+      pcl::fromROSMsg(vec_plane.vecPlane[i].cloud, vsp_plane[i].cloud);
+
+      vsp_plane[i].camera_position = camera_position;
+      vsp_plane[i].computeStatistics();
+      vsp_plane[i].computePlaneInfo();
+    }
+  }
+
+  void send2robot(stair_perception::Stair &stair, ros::Time ctime) {
+    double height_now, depth_now, hd_now, vd_now;
+
+    using namespace stair_perception;
+    std::shared_ptr<Node> pnext;
+    stair.reset();
+
+    while (stair.readNext(pnext)) {
+      if (pnext->pcurrent_type == step_node) {
+        std::shared_ptr<Node> &pstep = pnext;
+        height_now = pstep->step.height;
+        depth_now = pstep->step.depth;
+        hd_now = pstep->step.line.h;
+        vd_now = pstep->step.line.d;
+
+        if (!pstep->step.good_h) return;
+
+        break;
+      } else if (pnext->pcurrent_type == concaveline_node) {
+        std::shared_ptr<Node> &pline = pnext;
+      }
+    }
+
+    vec_time.push_back(ctime);
+    vec_height.push_back(height_now);
+    vec_depth.push_back(depth_now);
+
+    if (count == 1) {
+      height = height_now;
+      depth = depth_now;
+      hd = hd_now;
+      vd = vd_now;
+      ws = 1;
+    } else {
+      int start_idx = 0;
+      for (auto i = 0; i < vec_time.size() - 1; i++) {
+        if (fabs(fabs(ctime.toNSec() - vec_time[i].toNSec()) -
+                 time_window * 1000000000ull) < 0.05) {
+          start_idx = i;
+          break;
+        }
+      }
+      int bak_start_index = start_idx;
+
+      if (start_idx == 0) start_idx = vec_time.size() - 1 - ws;
+
+      ws = vec_time.size() - 1 - start_idx;
+
+      double sum_h = 0, sum_d = 0, cnt = 0;
+      for (auto i = start_idx; i < vec_time.size(); i++) {
+        if (fabs(height - vec_height[i]) > 0.04 ||
+            fabs(depth - vec_depth[i]) > 0.04)
+          continue;
+        cnt += 1;
+        sum_h += vec_height[i];
+        sum_d += vec_depth[i];
+      }
+
+      if (cnt) {
+        height = sum_h / cnt;
+        depth = sum_d / cnt;
+      }
+
+      if (fabs(height - height_now) < 0.04 && fabs(depth - depth_now) < 0.04) {
+        hd = hd_now;
+        vd = vd_now;
+      }
+
+      // remove outdated data
+      if (bak_start_index > 0) {
+        vec_time.erase(vec_time.begin(), vec_time.begin() + bak_start_index);
+        vec_height.erase(vec_height.begin(),
+                         vec_height.begin() + bak_start_index);
+        vec_depth.erase(vec_depth.begin(), vec_depth.begin() + bak_start_index);
+      }
+    }
+
+    output_record << ctime << "," << height << "," << depth << "," << hd << ","
+                  << vd << endl;
+    stair_info_msg::stair_info msg;
+    msg.has_stair = has_stair;
+    msg.height = height;
+    msg.depth = depth;
+    msg.v_height = hd;
+    msg.v_depth = vd;
+
+    pub_stair_info.publish(msg);
+  }
+
+  void publish_rviz_stair(
+      const std::vector<stair_perception::Plane> &vsp_plane_viz, ros::Time t) {
+    jsk_recognition_msgs::PolygonArray array_msg;
+    array_msg.header.stamp = t;
+    array_msg.header.frame_id = "t265_odom_frame";
+
+    if (!vsp_plane_viz.empty()) {
+      for (unsigned int i = 0; i < vsp_plane_viz.size(); i++) {
+        if ((vsp_plane_viz)[i].ptype ==
+            stair_perception::Plane::Ptype::stair_component) {
+          geometry_msgs::PolygonStamped polygon_stamped;
+          polygon_stamped.header.stamp = t;
+          polygon_stamped.header.frame_id = "t265_odom_frame";
+          if (vsp_plane_viz[i].counter.points.size() == 4) {
+            for (unsigned int j = 0; j < 4; j++) {
+              geometry_msgs::Point32 point32;
+              point32.x = vsp_plane_viz[i].counter.points[j].x;
+              point32.y = vsp_plane_viz[i].counter.points[j].y;
+              point32.z = vsp_plane_viz[i].counter.points[j].z;
+              polygon_stamped.polygon.points.push_back(point32);
+            }
+          }
+          array_msg.polygons.push_back(polygon_stamped);
+        } else if ((vsp_plane_viz)[i].ptype ==
+                   stair_perception::Plane::Ptype::others) {
+        } else if ((vsp_plane_viz)[i].ptype ==
+                   stair_perception::Plane::Ptype::ground) {
+        } else {
+          // pstair_component
+        }
+      }
+    }
+    pub_stair_poly.publish(array_msg);
+  }
+
+ private:
+  int count;
+
+  double ws;  // window size
+  double time_window;
+  std::vector<ros::Time> vec_time;
+  std::vector<double> vec_height, vec_depth;
+  double height, depth, hd, vd;
+  std::ofstream time_record, stair_record, output_record;
+
+  /*********** ros related ***********/
+  uint32_t queueSize;
+  ros::NodeHandle private_nh;
+
+  ros::Publisher pub_stair_info, pub_stair_poly;
+
+  std::vector<stair_perception::Plane> vsp_plane;
+
+  /*********** stair modeling ***********/
+  stair_perception::StairDetection stair_detector;
+  stair_perception::KeyInfo keyInfo;
+  bool has_stair;
+
+  std::string detial_stair_model, est_stair_param, running_time_str;
+};
+
+#endif
---
+++ stair_modeling_ros/src/stair_modeling_node.cpp
@@ -0,0 +1,264 @@
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <message_filters/sync_policies/exact_time.h>
+#include <message_filters/time_synchronizer.h>
+#include <nav_msgs/Odometry.h>
+#include <pcl/common/common.h>
+#include <pcl/io/openni_grabber.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/image_encodings.h>
+
+#include <Eigen/Eigen>
+#include <backward.hpp>
+#include <chrono>
+
+#include "AHCPlaneFitter.hpp"
+#include "plane_msg/Plane.h"
+#include "plane_msg/VecPlane.h"
+#include "stair_modeling/StairModeling.hpp"
+
+namespace backward {
+backward::SignalHandling sh;
+}
+
+using namespace std;
+using namespace std::chrono;
+struct OrganizedImage3D {
+  const cv::Mat_<cv::Vec3f>& cloud;
+  // note: ahc::PlaneFitter assumes mm as unit!!!
+  OrganizedImage3D(const cv::Mat_<cv::Vec3f>& c) : cloud(c) {}
+  inline int width() const { return cloud.cols; }
+  inline int height() const { return cloud.rows; }
+  inline bool get(const int row, const int col, double& x, double& y,
+                  double& z) const {
+    const cv::Vec3f& p = cloud.at<cv::Vec3f>(row, col);
+    x = p[0];
+    y = p[1];
+    z = p[2];
+    return z > 0 && isnan(z) == 0;  // return false if current depth is NaN
+  }
+};
+typedef ahc::PlaneFitter<OrganizedImage3D> PlaneFitter;
+
+class peac_ros {
+ public:
+  peac_ros(const ros::NodeHandle& private_nh = ros::NodeHandle("~"))
+      : private_nh_(private_nh), it_(private_nh), sm(private_nh) {
+    pub_vec_planes_ = private_nh_.advertise<plane_msg::VecPlane>("vecPlane", 5);
+    pub_pcl2_depth_ =
+        private_nh_.advertise<sensor_msgs::PointCloud2>("depth_pcl", 5);
+    pub_img_planes_ = it_.advertise("vec_plane_img", 1);
+
+    depth_sub_.reset(new message_filters::Subscriber<sensor_msgs::Image>(
+        private_nh_, "/d400/depth/image_rect_raw", 50));
+    odom_sub_.reset(new message_filters::Subscriber<nav_msgs::Odometry>(
+        private_nh_, "/t265/odom/sample", 100));
+    sync_image_odom_.reset(
+        new message_filters::Synchronizer<SyncPolicyImageOdom>(
+            SyncPolicyImageOdom(100), *depth_sub_, *odom_sub_));
+    sync_image_odom_->registerCallback(
+        boost::bind(&peac_ros::depthOdomCallback, this, _1, _2));
+
+    sm.init();
+  }
+
+  void depthOdomCallback(const sensor_msgs::ImageConstPtr& depth_msg,
+                         const nav_msgs::OdometryConstPtr& odom) {
+    // Get d400 pose in world (t265 as body)
+    Eigen::Quaterniond body_rotation_q = Eigen::Quaterniond(
+        odom->pose.pose.orientation.w, odom->pose.pose.orientation.x,
+        odom->pose.pose.orientation.y, odom->pose.pose.orientation.z);
+    Eigen::Matrix3d body_rotation_m = body_rotation_q.toRotationMatrix();
+    Eigen::Matrix4d body2world;  // from world to body
+    body2world.block<3, 3>(0, 0) = body_rotation_m;
+    body2world(0, 3) = odom->pose.pose.position.x;
+    body2world(1, 3) = odom->pose.pose.position.y;
+    body2world(2, 3) = odom->pose.pose.position.z;
+    body2world(3, 3) = 1.0;
+
+    Eigen::Matrix4d cam2body;  // from body to cam
+    Eigen::Quaterniond cam2body_rotation_q =
+        Eigen::Quaterniond(0.500, -0.500, 0.500, -0.500);
+    cam2body.block<3, 3>(0, 0) = cam2body_rotation_q.toRotationMatrix();
+    cam2body(0, 3) = -0.009375589;
+    cam2body(1, 3) = 0.015890727;
+    cam2body(2, 3) = 0.028273059;
+    cam2body(3, 3) = 1.0;
+
+    Eigen::Matrix4d cam_world = body2world * cam2body;
+    Eigen::Vector3d camera_pos;
+    camera_pos(0) = cam_world(0, 3);
+    camera_pos(1) = cam_world(1, 3);
+    camera_pos(2) = cam_world(2, 3);
+    Eigen::Quaterniond camera_rotation_q =
+        Eigen::Quaterniond(cam_world.block<3, 3>(0, 0));
+    Eigen::Matrix3d camera_rotation_m = camera_rotation_q.toRotationMatrix();
+
+    cv::Mat depth;
+    cv_bridge::CvImagePtr cv_ptr;
+    try {
+      cv_ptr = cv_bridge::toCvCopy(depth_msg, depth_msg->encoding);
+      if (depth_msg->encoding == sensor_msgs::image_encodings::TYPE_32FC1) {
+        (cv_ptr->image).convertTo(cv_ptr->image, CV_16UC1, 1000.0);
+      }
+      cv_ptr->image.copyTo(depth);
+    } catch (cv_bridge::Exception& e) {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return;
+    }
+
+    // configuration: 640*480 D435 & T265
+    // const float fx = 384.6916198730469;
+    // const float fy = 384.6916198730469;
+    // const float cx = 320.2483215332031;
+    // const float cy = 239.5032501220703;
+
+    // configuration: 848*480 D435 & T265
+    const float fx = 424.763671875;
+    const float fy = 424.763671875;
+    const float cx = 424.27420043945310;
+    const float cy = 239.45150756835938;
+    const float min_use_range = 0.1;  // unit: m
+    const float max_use_range = 3.5;  // unit: m
+
+    // only for visulization
+    pcl::PointCloud<pcl::PointXYZ> pcl_viz;
+
+    cv::Mat_<cv::Vec3f> cloud(depth.rows, depth.cols);
+    for (int r = 0; r < depth.rows; r++) {
+      uint16_t* depth_ptr = depth.ptr<uint16_t>(r);
+      cv::Vec3f* pt_ptr = cloud.ptr<cv::Vec3f>(r);
+      for (int c = 0; c < depth.cols; c++) {
+        float z = (float)depth_ptr[c] / 1000.0;
+
+        if (z < min_use_range || z > max_use_range) z = 0;
+
+        Eigen::Vector3d proj_pt;
+        proj_pt(0) = (c - cx) * z / fx;
+        proj_pt(1) = (r - cy) * z / fy;
+        proj_pt(2) = z;
+        // note: Do not transform before PlaneFitter!!!
+        pt_ptr[c][0] = proj_pt(0) * 1000.0;  // unit: m->mm
+        pt_ptr[c][1] = proj_pt(1) * 1000.0;  // unit: m->mm
+        pt_ptr[c][2] = proj_pt(2) * 1000.0;  // unit: m->mm
+
+        // only for visulization
+        if (z > 0.0) {
+          proj_pt = camera_rotation_m * proj_pt + camera_pos;
+          pcl::PointXYZ pt;
+          pt.x = proj_pt(0);
+          pt.y = proj_pt(1);
+          pt.z = proj_pt(2);
+          pcl_viz.points.push_back(pt);
+        }
+      }
+    }
+
+    // only for visulization
+    if (pub_pcl2_depth_.getNumSubscribers() > 0) {
+      pcl_viz.width = pcl_viz.points.size();
+      pcl_viz.height = 1;
+      pcl_viz.is_dense = true;
+      sensor_msgs::PointCloud2 pcl_ros;
+      pcl::toROSMsg(pcl_viz, pcl_ros);
+      pcl_ros.header.frame_id = "t265_odom_frame";
+      pcl_ros.header.stamp = depth_msg->header.stamp;
+      pub_pcl2_depth_.publish(pcl_ros);
+    }
+
+    // #TODO fix
+    pf.minSupport = 2000;
+    pf.windowWidth = 8;
+    pf.windowHeight = 8;
+    pf.doRefine = true;
+
+    auto t1 = high_resolution_clock::now();
+    std::vector<std::vector<int>> Membership;
+    cv::Mat Seg(depth.rows, depth.cols, CV_8UC3);
+    OrganizedImage3D Ixyz(cloud);
+    pf.run(&Ixyz, &Membership, &Seg, 0, true);
+    auto duration =
+        duration_cast<microseconds>(high_resolution_clock::now() - t1);
+    cout << "PlaneFitter: " << duration.count() / 1000.0 << " ms" << endl;
+
+    if (pub_img_planes_.getNumSubscribers() > 0) {
+      cv_bridge::CvImage out_msg;
+      out_msg.header = depth_msg->header;
+      out_msg.encoding = sensor_msgs::image_encodings::BGR8;
+      out_msg.image = Seg;
+      pub_img_planes_.publish(out_msg.toImageMsg());
+    }
+
+    // GenerateVecPlanes
+    plane_msg::VecPlane vecPlane;
+
+    size_t plane_number = Membership.size();
+    vecPlane.vecPlane.resize(plane_number);
+    vecPlane.header.stamp = depth_msg->header.stamp;
+    vecPlane.header.frame_id = "t265_odom_frame";
+
+    for (size_t i = 0; i < plane_number; i++) {
+      pcl::PointCloud<pcl::PointXYZ> pclcloud;
+      uint32_t point_number = Membership[i].size();
+      for (size_t j = 0; j < point_number; j++) {
+        int index = Membership[i][j];
+        int i_x = index / depth.cols;
+        int i_y = index % depth.cols;
+        double x, y, z;
+        Ixyz.get(i_x, i_y, x, y, z);
+        Eigen::Vector3d proj_pt(x / 1000.0, y / 1000.0, z / 1000.0);
+        proj_pt = camera_rotation_m * proj_pt + camera_pos;
+        pclcloud.points.push_back(
+            pcl::PointXYZ(proj_pt(0), proj_pt(1), proj_pt(2)));
+      }
+      pclcloud.width = point_number;
+      pclcloud.height = 1;
+
+      pcl::toROSMsg(pclcloud, vecPlane.vecPlane[i].cloud);
+      vecPlane.vecPlane[i].cloud.header = vecPlane.header;
+    }
+
+    pub_vec_planes_.publish(vecPlane);
+
+    // stair modeling
+    sm.planeOdomCallback(vecPlane, odom);
+  }
+
+  // data structure
+  typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image,
+                                                          nav_msgs::Odometry>
+      SyncPolicyImageOdom;
+  typedef shared_ptr<message_filters::Synchronizer<SyncPolicyImageOdom>>
+      SynchronizerImageOdom;
+  shared_ptr<message_filters::Subscriber<sensor_msgs::Image>> depth_sub_;
+  shared_ptr<message_filters::Subscriber<nav_msgs::Odometry>> odom_sub_;
+  SynchronizerImageOdom sync_image_odom_;
+
+  ros::NodeHandle private_nh_;
+  ros::Publisher pub_vec_planes_;
+  ros::Publisher pub_pcl2_depth_;
+
+  image_transport::ImageTransport it_;
+  image_transport::Publisher pub_img_planes_;
+
+  PlaneFitter pf;
+  StairModeling sm;
+};
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "demo_peac");
+  ros::NodeHandle private_nh("~");
+
+  peac_ros peacRos(private_nh);
+
+  ros::spin();
+  ros::shutdown();
+  return 0;
+}
